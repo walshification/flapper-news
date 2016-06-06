80a4dfba37bd08bc6654088830a118176e23af77
Christopher Walsh <walshification@gmail.com>
2016-06-06 06:30:57 -0500

    take user input for new post titles

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ angular.module('flapperNews', [])
       {title: 'post 4', upvotes: 9},
       {title: 'post 5', upvotes: 4},
     ]
+
+    $scope.addPost = function() {
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.title = '';
+    };
   }
 ]);
-
