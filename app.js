2d6f49db57b4340e5dda6941c2a04e1004f84ac5
Christopher Walsh <walshification@gmail.com>
2016-06-06 07:12:20 -0500

    extracts persistent posts to a service

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 angular.module('flapperNews', [])
-.controller('MainCtrl', [
-  '$scope',
-  function($scope){
-    $scope.posts = [
+.factory('posts', [function(){
+  var o = {
+    posts: [
       {title: 'post 1', upvotes: 5},
       {title: 'post 2', upvotes: 2},
       {title: 'post 3', upvotes: 15},
       {title: 'post 4', upvotes: 9},
       {title: 'post 5', upvotes: 4},
     ]
+  };
+  return o;
+}])
+.controller('MainCtrl', [
+  '$scope',
+  'posts',
+  function($scope, posts){
+    $scope.posts = posts.posts;
 
     $scope.addPost = function() {
       if(!$scope.title || $scope.title === '') { return; }
