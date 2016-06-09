08a8f09e426738bc9e70aec6a67c8c14908e0db9
Christopher Walsh <walshification@gmail.com>
2016-06-08 21:47:53 -0500

    wires up comment creation and upvoting

--- app/assets/javascripts/posts/postsCtrl.js
+++ app/assets/javascripts/posts/postsCtrl.js
@@ -3,17 +3,22 @@ angular.module('flapperNews')
   '$scope',
   'posts',
   'post',
-  function($scope, posts, post){
+  function($scope, posts, post) {
     $scope.post = post;
 
     $scope.addComment = function() {
       if($scope.body === '') { return; }
-      $scope.post.comments.push({
+      posts.addComment(post.id, {
         body: $scope.body,
         author: 'user',
-        upvotes: 0,
+      }).success(function(comment) {
+        $scope.post.comments.push(comment);
       });
       $scope.body = '';
+    };
+
+    $scope.incrementUpvotes = function(comment){
+      posts.upvoteComment(post, comment);
     };
   }
 ]);
